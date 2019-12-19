7a1d1d58b941502cacbcea149dd8a9dfeba4967b
adrianadermon <adrian.adermon@gmail.com>
2019-12-19 17:06:49 +0100

    Tweaks

--- push.sh
+++ push.sh
@@ -1,20 +1,10 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "adrian.adermon@gmail.com"
-  git config --global user.name "adrianadermon"
-}
+git config --global user.email "adrian.adermon@gmail.com"
+git config --global user.name "adrianadermon"
 
-commit_website_files() {
-  git add . CV.pdf
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-}
+git add . CV.pdf
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/adrianadermon/CV.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
-}
+git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME}.git 2>&1
 
-setup_git
-commit_website_files
-upload_files
