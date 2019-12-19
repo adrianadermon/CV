860b0f1c05239666a6c262098bb89db7dd44f0df
adrianadermon <adrian.adermon@gmail.com>
2019-12-19 18:25:20 +0100

    Git tests

--- push.sh
+++ push.sh
@@ -3,8 +3,16 @@
 git config --global user.email "adrian.adermon@gmail.com"
 git config --global user.name "adrianadermon"
 
+git status
+
+git checkout master
+
+git status
+
 git add . CV.pdf
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+
+git status
 
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME}.git 2>&1
 
