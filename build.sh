dc923dfc0ff84b9bd897cfa43a5d7b7149520d10
adrianadermon <adrian.adermon@gmail.com>
2019-12-20 08:53:21 +0100

    Tweak

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pandoc_options=--standalone --metadata date="`date "+%b %e, %y"`"
-latex_options=--latex-engine=xelatex --include-in-header=latex_header.tex --variable=subparagraph --variable papersize=a4
+PANDOC_OPTIONS=--standalone --metadata date="`date "+%b %e, %y"`"
+LATEX_OPTIONS=--latex-engine=xelatex --include-in-header=latex_header.tex --variable=subparagraph --variable papersize=a4
 
 pandoc $PANDOC_OPTIONS $LATEX_OPTIONS -o CV.pdf CV.md
