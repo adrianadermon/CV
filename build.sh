67f6213fc937e322b6191fcc6b6d25e7b81a0bea
adrianadermon <adrian.adermon@gmail.com>
2019-12-20 10:37:26 +0100

    Verbose Pandoc build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pandoc --standalone --metadata date="`date "+%b %e, %y"`" --latex-engine=xelatex --include-in-header=latex_header.tex --variable=subparagraph --variable papersize=a4 -o CV.pdf CV.md
+pandoc --standalone --metadata date="`date "+%b %e, %y"`" --latex-engine=xelatex --include-in-header=latex_header.tex --variable=subparagraph --variable papersize=a4 --verbose -o CV.pdf CV.md
